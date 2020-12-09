4332fb5b56e6d414efeb0f59502046318b787116
Omar Galvez <itsomargalvez@gmail.com>
2020-12-09 21:41:51 +0100

    js form validation add

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Selectors
+// Navbar Selectors
 
 let header = document.querySelector('.header');
 let hamburgerMenu = document.querySelector('.hamburger-menu');
@@ -15,4 +15,11 @@ hamburgerMenu.addEventListener('click', function () {
 
 navList.addEventListener('click', function () {
     header.classList.toggle('menu-open');
-})
+});
+
+
+// Form validation
+function lettersOnly(input) {
+    var regex = /[^a-z]/gi;  // regular expression that apply global flag along with an ignore case flag
+    input.value = input.value.replace(regex, "");
+};
