3c9b740718ab233f27099fe650ad2c29449cb56a
Omar Galvez <itsomargalvez@gmail.com>
2020-12-28 20:50:28 +0100

    impressum complete

--- main.js
+++ main.js
@@ -23,33 +23,3 @@ function lettersOnly(input) {
     var regex = /[^a-z]/gi;  // regular expression that apply global flag along with an ignore case flag
     input.value = input.value.replace(regex, "");
 };
-
-
-
-/*const form = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const subject = document.getElementById('subject')
-const message = document.getElementById('message')
-
-function Validate() {
-    // for Valid Characters i.e. Alphabets, Numbers and Space.
-    var pattern = /^[A-Za-z0-9 ]+$/
-
-    //Validate TextBox value against the Regex.
-    var isValid = pattern.test(document.getElementById('subject').value);
-    if (!isValid) {
-        alert = ("Please enter Valid Subject");
-    } else {
-        //alert("Does not contain Special Characters.");
-    }
-
-    return isValid;
-
-    var isValid = pattern.test(document.getElementById("message").value);
-    if (!isValid) {
-        alert = ("Please enter Valid Message");
-    }
-
-};
-*/
